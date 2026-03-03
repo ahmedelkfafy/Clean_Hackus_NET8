394c0d3caa4b5dbc0e0d86467e84e41c4096f856
Masoud <moattya.587@gmail.com>
2026-03-03 22:03:36 +0200

    Initial commit

---
+++ Models/Enums/CheckerState.cs
@@ -0,0 +1,9 @@
+namespace Clean_Hackus_NET8.Models.Enums;
+
+public enum CheckerState
+{
+    Stopped,
+    Running,
+    Paused,
+    Closing
+}
---
+++ Models/Enums/OperationResult.cs
@@ -0,0 +1,15 @@
+namespace Clean_Hackus_NET8.Models.Enums;
+
+public enum OperationResult
+{
+    Ok,
+    Bad,
+    Error,
+    HttpError,
+    ReCaptcha,
+    Blocked,
+    TwoFactor,
+    Multipassword,
+    Captcha,
+    HostNotFound
+}
