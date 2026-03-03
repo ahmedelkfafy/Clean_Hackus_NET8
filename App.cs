09ebfdb029a5f86f91a58268a53bb2a264910f82
Masoud <moattya.587@gmail.com>
2026-03-03 22:50:58 +0200

    Update App.cs

--- App.cs
+++ App.cs
@@ -5,11 +5,4 @@ namespace Clean_Hackus_NET8;
 
 public partial class App : Application
 {
-    [STAThread]
-    public static void Main()
-    {
-        var app = new App();
-        app.InitializeComponent();
-        app.Run();
-    }
 }
